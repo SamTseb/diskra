975543178f3e40cb4f9c7c8619075aaa1d445319
LampaTS <35936107+LampaTS@users.noreply.github.com>
2018-03-22 19:35:23 +0300

    Evgen <3

---
+++ celoe.h
@@ -0,0 +1,61 @@
+#pragma once
+// Ковалева, Иванов
+class celoe
+{
+public:
+	bool Is_Posotive;
+	int len;
+	int bdy[100];
+
+	celoe ABS_Z_N(celoe c1) // модуль
+	{
+		return;
+	}
+
+	int POZ_Z_D(celoe c1) // определение знака
+	{
+		return;
+	}
+
+	celoe MUL_ZM_Z(celoe c1) //умножение на -1
+	{
+		return;
+	}
+
+	celoe TRANS_N_Z(natural n1) //перевод в целое из натурального
+	{
+		return;
+	}
+
+	natural TRANS_Z_N(celoe c1) // перевод в натуральное
+	{
+		return;
+	}
+
+	celoe ADD_ZZ_Z(celoe c1, celoe c2) // сложение
+	{
+		return;
+	}
+
+	celoe SUB_ZZ_Z(celoe c1, celoe c2)// вычитание
+	{
+		return;
+	}
+
+	celoe MUL_ZZ_Z(celoe c1, celoe c2)// умножение
+	{
+		return;
+	}
+
+	celoe DIV_ZZ_Z(celoe c1, celoe c2)//частное от деление
+	{
+		return;
+	}
+
+	celoe MOD_ZZ_Z(celoe c1, celoe c2)// остаток от деления
+	{
+		return;
+	}
+
+
+};
