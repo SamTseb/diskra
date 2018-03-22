5d1273c86e8e9bc7eed63c14e945e2a43bc7ba4a
LampaTS <maksim.kopylov@gmail.com>
2018-03-22 19:02:12 +0300

    Дроби Шаблон

---
+++ fraction.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "celoe.h"
+// Хохлов, Цебульский
+class fraction
+{
+public:
+	int chisl_len;
+	int chisl[100];
+	int znam_len;
+	int znam[100];
+
+	fraction RED_Q_Q(fraction f1) // сокращение дроби
+	{
+		return;
+	};
+
+	bool INT_Q_B(fraction f1) // проверка не целое
+	{
+		return;
+	};
+
+	fraction TRANS_Z_Q(celoe c1) // целое в дробь
+	{
+		return;
+	};
+
+	fraction TRANS_Q_Z(fraction f1) // дробь в целое
+	{
+		return;
+	};
+
+	fraction ADD_QQ_Q(fraction f1) // сложение
+	{
+		return;
+	};
+
+	fraction SUB_QQ_Q(fraction f1) // вычитание
+	{
+		return;
+	};
+
+	fraction MUL_QQ_Q(fraction f1) // умножение 
+	{
+		return;
+	};
+
+	fraction DIV_QQ_Q(fraction f1) // деление
+	{
+		return;
+	};
+};
